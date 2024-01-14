ff2ccdd53d80e8d5e7e6ca3afa158ad2222fe48e
Leonardo Daniel Palomino Condor <130775080+LeonardoPalominoCondor@users.noreply.github.com>
2024-01-13 20:48:05 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig ={
-  output:'export',
+const nextConfig = {
+  output: 'export',
+  // Otras opciones de configuración según sea necesario
 }
+
 module.exports = {
+  ...nextConfig,
   async redirects() {
     return [
       {
@@ -10,6 +13,7 @@ module.exports = {
         destination: '/trabajos/[week]',
         permanent: true,
       },
+      // Puedes agregar más redirecciones según sea necesario
     ];
   },
 };
